b25c9939b1d7dc5780430d9a85f2f1d2f4e4f07f
Jesse Stricker <me@jessestricker.de>
2020-01-22 18:38:09 +0100

    Add application entrypoint

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// phase 0: get command line arguments
+	// phase 1: get list of files to check
+	// phase 2: compute groups of duplicate files
+	// phase 3: present result to user
+}
